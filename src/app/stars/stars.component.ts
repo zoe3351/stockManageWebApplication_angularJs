6ff15264c4ced1c13e577ce25f8a958f42f14c49
dongjin <shixi_dongjin@staff.sina.com>
2017-07-17 17:28:49 +0800

    add components

---
+++ src/app/stars/stars.component.ts
@@ -0,0 +1,24 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-stars',
+  templateUrl: './stars.component.html',
+  styleUrls: ['./stars.component.css']
+})
+export class StarsComponent implements OnInit {
+
+  @Input()
+  rating: number = 0;
+
+  stars: boolean[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.stars = [];
+    for (let i = 1; i <= 5; i++) {
+      this.stars.push(i > this.rating);
+    }
+  }
+
+}
---
+++ src/app/stock/stock-manage/stock-manage.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-manage',
+  templateUrl: './stock-manage.component.html',
+  styleUrls: ['./stock-manage.component.css']
+})
+export class StockManageComponent implements OnInit {
+  private stocks: Array<Stock>;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.stocks = [
+      new Stock(1, 'the first stock', 1.99, 3.5, 'this is the first stock', ['IT', 'ANGU']),
+      new Stock(2, 'the second stock', 2.99, 2.5, 'this is the second stock', ['IT', 'BAT']),
+      new Stock(3, 'the third stock', 3.99, 1.5, 'this is the third stock', ['IT', 'FIN']),
+      new Stock(4, 'the forth stock', 4.99, 4.5, 'this is the forth stock', ['IT', 'INTERNET']),
+      new Stock(5, 'the fifth stock', 3.99, 3.5, 'this is the fifth stock', ['IT', 'ECON'])
+    ];
+  }
+
+}
+
+export class Stock {
+  constructor(
+    public id: number,
+    public name: string,
+    public price: number,
+    public rating: number,
+    public desc: string,
+    public categories: Array<string>) {
+  }
+}
