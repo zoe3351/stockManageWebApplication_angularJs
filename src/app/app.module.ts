0bc0c5fb8de02b24b3cd5545c0e4aef5cfbada1e
dongjin <jd62@uw.edu>
2017-07-25 22:56:53 +0800

    using pipe & reactive programming  to supper stock search function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {HttpModule} from '@angular/http';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
 import { MenuComponent } from './menu/menu.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
@@ -15,6 +15,7 @@ import {RouterModule, Routes} from "@angular/router";
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { StockFormComponent } from './stock/stock-form/stock-form.component';
 import {StockService} from "./stock/stock.service";
+import { StockFilterPipe } from './stock/stock-filter.pipe';
 
 const routeConfig: Routes = [
   {path: '', redirectTo: '/dashboard', pathMatch:'full'},
@@ -34,12 +35,14 @@ const routeConfig: Routes = [
     StockManageComponent,
     StarsComponent,
     DashboardComponent,
-    StockFormComponent
+    StockFormComponent,
+    StockFilterPipe
   ],
   imports: [
     BrowserModule,
     HttpModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(routeConfig)
   ],
   providers: [StockService],
--- src/app/stock/stock-manage/stock-manage.component.ts
+++ src/app/stock/stock-manage/stock-manage.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
 import {Stock, StockService} from "../stock.service";
+import {FormControl} from "@angular/forms";
+import 'rxjs/Rx';
 
 @Component({
   selector: 'app-stock-manage',
@@ -10,11 +12,17 @@ import {Stock, StockService} from "../stock.service";
 export class StockManageComponent implements OnInit {
   private stocks: Array<Stock>;
 
+  private nameFilter: FormControl = new FormControl();
+
+  private keyWord:string;
+
   constructor(public router:Router, private stockService : StockService) { }
 
   ngOnInit() {
     this.stocks = this.stockService.getStocks();
-
+    this.nameFilter.valueChanges
+      .debounceTime(500)
+      .subscribe(value => this.keyWord = value);
   }
 
   create(){
