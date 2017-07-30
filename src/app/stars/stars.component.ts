8f4b867cf7d2a0c3212a3e9b1b434b2d00fde5c3
dongjin <jd62@uw.edu>
2017-07-30 22:49:47 +0800

    build communication between components

--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -1,24 +1,45 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChange, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-stars',
   templateUrl: './stars.component.html',
   styleUrls: ['./stars.component.css']
 })
-export class StarsComponent implements OnInit {
+export class StarsComponent implements OnInit, OnChanges{
+
+
 
   @Input()
   rating: number = 0;
 
   stars: boolean[];
 
+  @Input()
+  readonly: boolean = true;
+
+  @Output()
+  ratingChange: EventEmitter<number> = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
     this.stars = [];
     for (let i = 1; i <= 5; i++) {
       this.stars.push(i > this.rating);
     }
+  }
+
+  clickStar(index: number) {
+    if (!this.readonly){
+      this.rating = index + 1;
+
+      this.ratingChange.emit(this.rating);
+    }
+
   }
 
 }
